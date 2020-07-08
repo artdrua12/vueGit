c737fc15d20e1e416dfb77a7e446cb152e878449
Alex <pasechnyy@proitr.ru>
2020-07-08 17:08:29 +0300

    debouce

--- src/App.vue
+++ src/App.vue
@@ -4,57 +4,39 @@
       app
       color="primary"
       dark
+      shrink-on-scroll
+      prominent
+      src="@/assets/flower.jpg"
+      fade-img-on-scroll
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Навигация</v-toolbar-title>
+      <template v-slot:extension>
+        <v-tabs>
+          <v-tab to="/">Home</v-tab>
+          <v-tab to="/about">About</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+<style>
+.app__fullWidth{
+  grid-column: -1/1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="home">
-    <h1>Введите URL</h1>
-    <input v-model="users" placeholder="Enter URL" @keyup="keyup" />
-    <div v-for="item in searchUsers.items" :key="item.id" >
+    <h1 class="app__fullWidth">Введите имя пользователя Git</h1>
+    <div class="app__fullWidth home__header">
+      >
+      <v-text-field
+        v-model="users"
+        :rules="nameRules"
+        :counter="12"
+        label="Имя в Git"
+        required
+        @keyup="keyup"
+      ></v-text-field>
+      <v-pagination v-model="page" :length="7"></v-pagination>
+    </div>
+    <div v-for="item in searchUsers.items" :key="item.id" class="home__users">
       <h2>{{item.login}}</h2>
-    <img :src="item.avatar_url" width="200px">
+      <img :src="item.avatar_url" width="200px" />
     </div>
   </div>
 </template>
@@ -14,17 +25,27 @@ export default {
   data() {
     return {
       users: "",
+      page: 0,
+      nameRules: [
+        v => !!v || "Необходимо ввести имя",
+        v => v.length <= 12 || "Имя должно быть не более 12 символов"
+      ],
+      timeout: null
     };
   },
   computed: {
-    searchUsers(){
-      return this.$store.state.searchUsers
+    searchUsers() {
+      return this.$store.state.searchUsers;
     }
   },
   methods: {
     keyup() {
       console.log(" users", this.searchUsers);
-      this.$store.dispatch("search",this.users);
+      this.$store.dispatch("search", this.users);
+    },
+    debouce(fn) {
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(fn, 500);
     }
   },
   components: {}
@@ -33,7 +54,26 @@ export default {
 
 <style scoped>
 .home {
+  max-width: 1200px;
+  min-height: 100%;
+  margin: auto;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-rows: 1fr, auto;
+  grid-gap: 20px;
+  justify-items: center;
+  align-content: flex-start;
+  text-align: center;
+}
+.home__header {
+  width: 90%;
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+h1 {
+  margin-top: 32px;
 }
 </style>
